0d452eacaa1f4c36997622dca5e458b574bb2f34
rmscoal <rifkymanuelsatyana@proton.me>
2023-01-27 21:53:27 +0700

    Add Staying In Grid Leetcode with Python

--- python/index.py
+++ python/index.py
@@ -1,6 +1,8 @@
 from leetcode.pangramsentence import PangramSentence
 from leetcode.twostringarray import TwoStringArray
+from leetcode.stayingingrid import StayingInGrid
 
 if __name__ == "__main__":
   print("Pangram Sentence Solver: ", PangramSentence.Solver("thequickbrownfoxjumpsoverthelazydog"))
   print("Equivalence of Two String Array: ", TwoStringArray.Solver(["ab", "c"], ["a", "bc"]))
+  print("Staying In Grid: ", StayingInGrid.FastSolver(3, [0,1], "RRDDLU"))
