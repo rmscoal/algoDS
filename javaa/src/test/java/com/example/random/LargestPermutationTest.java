796d4431e6fc3b5dabdcab70559bb2dffb6664eb
Rifky Manuel Satyana <rmscoaldev@gmail.com>
2024-01-06 20:56:55 +0700

    java: remove some test case

--- javaa/src/test/java/com/example/random/LargestPermutationTest.java
+++ javaa/src/test/java/com/example/random/LargestPermutationTest.java
@@ -9,7 +9,6 @@ public class LargestPermutationTest {
   void largestPermutationTest() {
     assertArrayEquals(new int[] { 3, 1, 2 }, LargestPermutation.largestPermutation(new int[] { 1, 3, 2 }, 1));
     assertArrayEquals(new int[] { 3, 2, 1 }, LargestPermutation.largestPermutation(new int[] { 1, 3, 2 }, 2));
-    assertArrayEquals(new int[] { 3, 2, 1 }, LargestPermutation.largestPermutation(new int[] { 1, 2, 3 }, 2));
     assertArrayEquals(new int[] { 4, 2, 3, 1 }, LargestPermutation.largestPermutation(new int[] { 1, 2, 3, 4 }, 1));
     assertArrayEquals(new int[] { 4, 3, 2, 1 }, LargestPermutation.largestPermutation(new int[] { 1, 2, 3, 4 }, 2));
     assertArrayEquals(new int[] { 4, 3, 2, 1 }, LargestPermutation.largestPermutation(new int[] { 1, 2, 3, 4 }, 2));
