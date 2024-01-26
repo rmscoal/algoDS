69654bbb16badb93b5a9bd587325699ba6f9bb77
Rifky Manuel Satyana <rmscoaldev@gmail.com>
2024-01-27 00:35:41 +0700

    go(review): top k frequent cleaner

--- go/leetcode_v2/arrays/top_k_frequent_test.go
+++ go/leetcode_v2/arrays/top_k_frequent_test.go
@@ -36,3 +36,60 @@ func topKFrequent(nums []int, k int) []int {
 func TestTopKFrequent(t *testing.T) {
 	fmt.Printf("Result %+v\n", topKFrequent([]int{1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2}, 2))
 }
+
+func topKFrequent_Cleaner(nums []int, k int) []int {
+	sort.Ints(nums)
+
+	markedIndex := 0
+	count := 1
+	result := []int{}
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == nums[markedIndex] {
+			count++
+		} else if count >= k {
+			result = append(result, nums[markedIndex])
+			markedIndex = i
+			count = 1
+		}
+	}
+
+	if count >= k {
+		result = append(result, nums[markedIndex])
+	}
+
+	return result
+}
+
+func TestTopKFrequent_Cleaner(t *testing.T) {
+	testcases := []struct {
+		nums   []int
+		k      int
+		result []int
+	}{
+		{
+			nums:   []int{1, 1, 1, 2, 2, 3},
+			k:      2,
+			result: []int{1, 2},
+		},
+		{
+			nums:   []int{1},
+			k:      1,
+			result: []int{1},
+		},
+	}
+
+	for idx, tc := range testcases {
+		t.Run(fmt.Sprintf("Running testcase %d", idx), func(t *testing.T) {
+			result := topKFrequent_Cleaner(tc.nums, tc.k)
+			if len(result) != len(tc.result) {
+				t.Fatalf("Expected to be %v but got %v", tc.result, result)
+			}
+			for i, v := range tc.result {
+				if v != result[i] {
+					t.Fatalf("Expected to be %v but got %v", tc.result, result)
+				}
+			}
+		})
+	}
+}
